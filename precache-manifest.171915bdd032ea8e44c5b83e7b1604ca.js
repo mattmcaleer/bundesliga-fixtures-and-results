a7f5230626dc42a20952a28daa16d9ca3aefcf69
Matthew <matthew_mcaleer@hotmail.com>
2022-10-01 20:34:02 +1000

    Updates

---
+++ precache-manifest.171915bdd032ea8e44c5b83e7b1604ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42c0e648bef890ffbf7",
+    "url": "./static/css/main.44841cf8.chunk.css"
+  },
+  {
+    "revision": "d42c0e648bef890ffbf7",
+    "url": "./static/js/main.e11d03cf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8234444fecdae76b525e",
+    "url": "./static/js/2.fa415a2c.chunk.js"
+  },
+  {
+    "revision": "602753d4e90006126d7d539c40b25340",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bundesliga-fixtures-and-results/home/precache-manifest.d14068745d591756f3d82eaba3265541.js"
+  "./precache-manifest.171915bdd032ea8e44c5b83e7b1604ca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bundesliga-fixtures-and-results/home/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
