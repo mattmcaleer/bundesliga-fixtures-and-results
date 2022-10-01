ab986ce0dd1ed22183b3ef6b8ea95623480e4d75
Matthew <matthew_mcaleer@hotmail.com>
2022-10-01 19:08:24 +1000

    Updates

---
+++ precache-manifest.3ac695f3af3c647f55f48706a4c64704.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96d3d121e38690242e89",
+    "url": "/bundesliga-fixtures-and-results/static/css/main.d4554d08.chunk.css"
+  },
+  {
+    "revision": "96d3d121e38690242e89",
+    "url": "/bundesliga-fixtures-and-results/static/js/main.15208039.chunk.js"
+  },
+  {
+    "revision": "72f0edee31966cda6d80",
+    "url": "/bundesliga-fixtures-and-results/static/js/runtime~main.fafd52f8.js"
+  },
+  {
+    "revision": "9e246d9182520e62bd68",
+    "url": "/bundesliga-fixtures-and-results/static/js/2.4e5a4952.chunk.js"
+  },
+  {
+    "revision": "0b22e43e856a98f5725f0181a4fba59e",
+    "url": "/bundesliga-fixtures-and-results/index.html"
+  }
+];
