00ebece944c4d703fb8f952897011a124e7a6f25
Matthew <matthew_mcaleer@hotmail.com>
2022-10-01 20:46:18 +1000

    routes update

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="" element={<TitlePage />} />
-        <Route path="" element={<Results seasons={seasons} divisions={divisions} />} />
-        <Route path="" element={<LeagueTable seasons={seasons} divisions={divisions} />} />
+        <Route path="/fixtures" element={<Results seasons={seasons} divisions={divisions} />} />
+        <Route path="/table" element={<LeagueTable seasons={seasons} divisions={divisions} />} />
       </Routes>
     </div>
   );
