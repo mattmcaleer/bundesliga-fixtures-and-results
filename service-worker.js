44576e4f175b39c383d3c4fd87110271cd0e5edf
Matthew <matthew_mcaleer@hotmail.com>
2022-10-02 10:29:59 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9cf326802475094c4b851deb1dcf7852.js"
+  "/bundesliga-fixtures-and-results/precache-manifest.993ee2e239c26c3d4b1024fc120cda29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/bundesliga-fixtures-and-results/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
